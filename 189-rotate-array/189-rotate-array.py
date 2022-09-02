dbc0bca1c681fbba1cc25cc814a336bee28f6fbc
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2022-09-03 00:41:35 +0900

    Time: 346 ms (17.42%), Space: 25.1 MB (15.67%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        temp = [0] * len(nums)
+        
+        for i, ele in enumerate(nums):
+            temp[(i + k) % len(nums)] = nums[i]
+        
+        nums[:] = temp
