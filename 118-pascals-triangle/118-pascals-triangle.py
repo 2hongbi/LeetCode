ec8825ddabfb1af7cdc72183c748e92c953c3fad
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2022-09-03 00:26:37 +0900

    Time: 36 ms (16.69%), Space: 13.2 MB (85.95%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        pascal = []
+        
+        if numRows <= 0:
+            return pascal
+        
+        pascal.append([1])
+        
+        for i in range(1, numRows):
+            prev_len = len(pascal[i - 1])
+            curr_list = []
+            
+            for j in range(prev_len + 1):
+                num = 1
+                if j != 0 and j != prev_len:
+                    num = pascal[i - 1][j - 1] + pascal[i - 1][j]
+                
+                curr_list.append(num)
+            
+            pascal.append(curr_list)
+            
+        return pascal
