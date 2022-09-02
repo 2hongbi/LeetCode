524a6c4404c2302b5b0b697bfb5296de83073c9b
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2022-09-03 00:35:20 +0900

    Time: 286 ms (9.54%), Space: 14.5 MB (99.53%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -4,16 +4,4 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        majority_count = int(len(nums) / 2)
-    
-        hashmap = {}
-        
-        for num in nums:
-            if hashmap.get(num) != None:
-                hashmap[num] = hashmap[num] + 1
-            else:
-                hashmap[num] = 1
-            
-            if hashmap[num] > majority_count:
-                return num
-        return -1
+        return sorted(nums)[int(len(nums) / 2)]
