e3bc3854678199148cb1e26031b2b391c00f0db5
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2022-09-30 23:54:29 +0900

    Time: 93 ms (40.83%), Space: 14 MB (89.57%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,17 +1,29 @@
-import re
-
 class Solution(object):
     def isPalindrome(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        strs = []
-        for char in s:
-            if char.isalnum():
-                strs.append(char.lower())
-
-        while len(strs) > 1:
-            if strs.pop(0) != strs.pop():
+        i = 0
+        j = len(s) - 1
+        
+        s = s.lower()
+        
+        while i < j:
+            while i < j:
+                if s[i].isalnum():
+                    break
+                i += 1
+        
+            while i < j:
+                if s[j].isalnum():
+                    break
+                j -= 1
+            
+            if s[i] != s[j]:
                 return False
+        
+            i += 1
+            j -= 1
+
         return True
