6a699d1d8a03dbc6bd34265ff0463364237f4c0a
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2022-11-05 23:59:43 +0900

    Update 445-add-two-numbers-ii.py
    Reverse linked list

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.py
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.py
@@ -5,28 +5,50 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        num1_str = ""
-        num2_str = ""
+        def reverse(head):
+            prev = None
+            curr = head
 
-        l1_curr = l1
-        l2_curr = l2
+            while curr != None:
+                next_temp = curr.next
+                curr.next = prev
 
-        while l1_curr != None:
-            num1_str = num1_str + str(l1_curr.val)
-            l1_curr = l1_curr.next
+                prev = curr
+                curr = next_temp
+            return prev
 
-        while l2_curr != None:
-            num2_str = num2_str + str(l2_curr.val)
-            l2_curr = l2_curr.next
+        r_l1 = reverse(l1)
+        r_l2 = reverse(l2)
 
-        res_num = int(num1_str) + int(num2_str)
+        res_head = None
 
-        # dummy node
-        head = ListNode(-1)
-        curr = head
-        for num_ch in str(res_num):
-            curr.next = ListNode(int(num_ch))
-            curr = curr.next
+        carry = 0
+        while r_l1 != None or r_l2 != None:
+            num1 = 0
+            num2 = 0
 
-        curr.next = None
-        return head.next
+            if r_l1 != None:
+                num1 = r_l1.val
+                r_l1 = r_l1.next
+            
+            if r_l2 != None:
+                num2 = r_l2.val
+                r_l2 = r_l2.next
+
+            carry, num = divmod(num1 + num2 + carry, 10)
+
+            node = ListNode(num)
+            if res_head == None:
+                res_head = node
+            else:
+                temp = res_head
+                res_head = node
+                node.next = temp
+
+        if carry != 0:
+            node = ListNode
+            temp = res_head
+            res_head = node
+            node.next = temp
+
+        return res_head
